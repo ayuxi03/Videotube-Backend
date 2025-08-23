07048a6bc30e3cc6eb1ac51bca2dfa74017814e2
ayuxi03 <ayushimoitra03@gmail.com>
2025-08-24 00:54:35 +0530

    Multer middleware added

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -8,7 +8,8 @@ class ApiError extends Error {
     super(message)
     this.statusCode = statusCode
     this.data = null
-    this.message = messagethis.success = false;
+    this.message = message
+    this.success = false;
     this.errors = errors
 
     if (stack) {
