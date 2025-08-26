84469c8e13ac9077d1a56590ca2f72493d022c5c
ayuxi03 <ayushimoitra03@gmail.com>
2025-08-27 00:54:18 +0530

    Added register controller logic

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,8 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js"
+import { User } from "../models/user.model.js"
+import { uploadOnCloudinary } from "../utils/cloudinary.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
 
 const registerUser = asyncHandler( async (req, res) => {
   // get user details from frontend
@@ -17,12 +20,54 @@ const registerUser = asyncHandler( async (req, res) => {
 
   if (
     [fullName, email, username, password].some((field) => field?.trim() === "")) {
-      throw new ApiError(400, "All fields are required")
+      throw new ApiError(400, "All fields are required");
     }
 
-  // if (fullName === "") {
-  //   throw new ApiError(400, "fullname is required")
-  // }
+    const existingUser = User.findOne({
+      $or: [{username}, {email}]
+    })
+
+    if (existingUser) {
+      throw new ApiError(409, "User with email or username already exists");
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    console.log(req.files)
+    console.log(req.files.avatar)
+
+    if (!avatarLocalPath) {
+      throw new ApiError(404, "Avatar is required")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+    if (!avatar) {
+      throw new ApiError(404, "Avatar is required")
+    }
+
+    const user = await User.create({
+      fullName,
+      avatar: avatar.url,
+      coverImage: coverImage?.url || "",
+      email,
+      password,
+      username: username.toLowerCase()
+    })
+
+    const createdUser = await User.findById(user._id).select(
+      "-password -refreshToken"
+    )
+
+    if (!createdUser) {
+      throw new ApiError(500, "Something went wrong while registering the user");
+    }
+
+    return res.status(201).json(
+      new ApiResponse(200, createdUser, "User registered successfully")
+    )
 
 });
 
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -48,7 +48,7 @@ const userSchema = new Schema({
 
 userSchema.pre("save", async function(next) { // next is used to denote end of function and move on
   if (!this.isModified("password")) return next();
-  this.password = bcrypt.hash(this.password, 10)
+  this.password = await bcrypt.hash(this.password, 10)
   next()
 })
 
