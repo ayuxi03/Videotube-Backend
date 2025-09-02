6ca5d97edbfabf7ff72fc6224bdd4cff6588593f
ayuxi03 <ayushimoitra03@gmail.com>
2025-09-02 13:34:12 +0530

    Minor indentation fix

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -38,61 +38,61 @@ const registerUser = asyncHandler( async (req, res) => {
   if (
     [fullName, email, username, password].some((field) => field?.trim() === "")) {
       throw new ApiError(400, "All fields are required");
-    }
+  }
 
-    const existingUser = await User.findOne({
-      $or: [{username}, {email}]
-    })
+  const existingUser = await User.findOne({
+    $or: [{username}, {email}]
+  })
 
-    if (existingUser) {
-      throw new ApiError(409, "User with email or username already exists");
-    }
+  if (existingUser) {
+    throw new ApiError(409, "User with email or username already exists");
+  }
 
-    const avatarLocalPath = req.files?.avatar[0]?.path;
-    //const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  //const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
-    let coverImageLocalPath;
-    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
-      coverImageLocalPath = req.files.coverImage[0].path
-    }
+  let coverImageLocalPath;
+  if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+    coverImageLocalPath = req.files.coverImage[0].path
+  }
 
-    /*
-    console.log(req.files) // returns object with 2 arrays - avatar, coverImage. each array contains an object with all the details
-    console.log(req.files.avatar) // this is an array with an object containing all the details. Hence, avatar[0] is the object with the details
-    */
+  /*
+  console.log(req.files) // returns object with 2 arrays - avatar, coverImage. each array contains an object with all the details
+  console.log(req.files.avatar) // this is an array with an object containing all the details. Hence, avatar[0] is the object with the details
+  */
 
-    
-    if (!avatarLocalPath) {
-      throw new ApiError(404, "Avatar is required")
-    }
+  
+  if (!avatarLocalPath) {
+    throw new ApiError(404, "Avatar is required")
+  }
 
-    const avatar = await uploadOnCloudinary(avatarLocalPath);
-    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
 
-    if (!avatar) {
-      throw new ApiError(404, "Avatar is required")
-    }
+  if (!avatar) {
+    throw new ApiError(404, "Avatar is required")
+  }
 
-    const user = await User.create({
-      fullName,
-      avatar: avatar.url,
-      coverImage: coverImage?.url || "",
-      email,
-      password,
-      username: username.toLowerCase()
-    })
+  const user = await User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username: username.toLowerCase()
+  })
 
-    const createdUser = await User.findById(user._id).select(
-      "-password -refreshToken"
-    )
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  )
 
-    if (!createdUser) {
-      throw new ApiError(500, "Something went wrong while registering the user");
-    }
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong while registering the user");
+  }
 
-    return res.status(201).json(
-      new ApiResponse(200, createdUser, "User registered successfully")
-    )
+  return res.status(201).json(
+    new ApiResponse(200, createdUser, "User registered successfully")
+  )
 
 });
 
