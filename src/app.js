3eb76c13bd75dca1ff654fc2826c29195f1f87c6
ayuxi03 <ayushimoitra03@gmail.com>
2025-09-05 11:01:50 +0530

    Initiated video controller and route files

--- src/app.js
+++ src/app.js
@@ -17,12 +17,12 @@ app.use(cookieParser());
 // Routes
 import userRouter from "./routes/user.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
-
-
+import videoRouter from "./routes/video.routes.js";
 
 // Routes declaration
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/videos", videoRouter)
 
 //http://localhost:8000/api/v1/users/
 
