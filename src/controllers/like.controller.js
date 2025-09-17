549d77b4022c1d147bf69b9e51a17a96e9bf7156
ayuxi03 <ayushimoitra03@gmail.com>
2025-09-18 01:30:44 +0530

    Initiate like controller and routes

---
+++ src/controllers/like.controller.js
@@ -0,0 +1,27 @@
+import mongoose, { isValidObjectId } from "mongoose";
+import { Like } from "../models/like.model.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { isValidObjectId } from "mongoose";
+
+const toggleVideoLike = asyncHandler ( async (req, res) => {
+
+})
+
+
+const toggleCommentLike = asyncHandler ( async (req, res) => {
+  
+})
+
+
+const toggleTweetLike = asyncHandler ( async (req, res) => {
+  
+})
+
+
+const getLikedVideos = asyncHandler ( async (req, res) => {
+  
+})
+
+export { toggleCommentLike, toggleTweetLike, toggleVideoLike, getLikedVideos }
