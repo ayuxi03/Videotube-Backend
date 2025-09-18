ce59081e8af5cb9b07c9b9184e01f1da03f1e8e3
ayuxi03 <ayushimoitra03@gmail.com>
2025-09-19 00:01:57 +0530

    Added toggle like controllers

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -6,17 +6,122 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { isValidObjectId } from "mongoose";
 
 const toggleVideoLike = asyncHandler ( async (req, res) => {
+  const { videoId } = req.params;
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, "Invalid video ID")
+  }
 
+  const existingLike = await Like.findOne({
+    video: videoId,
+    likedBy: req.user?._id
+  })
+
+  if (existingLike) {
+    await Like.findByIdAndDelete({
+      _id: existingLike._id
+    })
+    
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(200, existingLike, "Video unliked successfully")
+      )
+  }
+
+  const newLike = await Like.create({
+    video: videoId,
+    likedBy: req.user?._id
+  })
+
+  if (!newLike) {
+    throw new ApiError(500, "Failed to like video")
+  }
+
+  return res
+    .status(201)
+    .json(
+      new ApiResponse(201, newLike, "Video liked successfully")
+    )
 })
 
 
 const toggleCommentLike = asyncHandler ( async (req, res) => {
-  
+  const { commentId } = req.params;
+  if (!isValidObjectId(commentId)) {
+    throw new ApiError(400, "Invalid comment ID")
+  }
+
+  const existingLike = await Like.findOne({
+    comment: commentId,
+    likedBy: req.user?._id
+  })
+
+  if (existingLike) {
+    await Like.findByIdAndDelete({
+      _id: existingLike._id
+    })
+    
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(200, existingLike, "Comment unliked successfully")
+      )
+  }
+
+  const newLike = await Like.create({
+    comment: commentId,
+    likedBy: req.user?._id
+  })
+
+  if (!newLike) {
+    throw new ApiError(500, "Failed to like comment")
+  }
+
+  return res
+    .status(201)
+    .json(
+      new ApiResponse(201, newLike, "Comment liked successfully")
+    )
 })
 
 
 const toggleTweetLike = asyncHandler ( async (req, res) => {
-  
+  const { tweetId } = req.params;
+  if (!isValidObjectId(tweetId)) {
+    throw new ApiError(400, "Invalid tweet ID")
+  }
+
+  const existingLike = await Like.findOne({
+    tweet: tweetId,
+    likedBy: req.user?._id
+  })
+
+  if (existingLike) {
+    await Like.findByIdAndDelete({
+      _id: existingLike._id
+    })
+    
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(200, existingLike, "Tweet unliked successfully")
+      )
+  }
+
+  const newLike = await Like.create({
+    tweet: tweetId,
+    likedBy: req.user?._id
+  })
+
+  if (!newLike) {
+    throw new ApiError(500, "Failed to like tweet")
+  }
+
+  return res
+    .status(201)
+    .json(
+      new ApiResponse(201, newLike, "Tweet liked successfully")
+    )
 })
 
 
