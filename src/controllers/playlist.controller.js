3afb28fa4fe227bd4a418dd05df5f75caf4322b3
ayuxi03 <ayushimoitra03@gmail.com>
2025-09-23 00:05:48 +0530

    Add delete and update playlist functionalities and routes

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -30,13 +30,60 @@ const createPlaylist = asyncHandler ( async (req, res) => {
 
 
 const deletePlaylist = asyncHandler ( async (req, res) => {
-  
+  const { playlistId } = req.params;
+  if (!isValidObjectId(playlistId)) {
+    throw new ApiError(400, "Invalid playlist ID");
+  }
+
+  const deletedPlaylist = await Playlist.findOneAndDelete({
+    _id: playlistId,
+    owner: req.user?._id
+  })
+
+  if (!deletedPlaylist) {
+    throw new ApiError(404, "Playlist not found or unauthorized to delete")
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, deletedPlaylist, "Playlist deleted successfully.")
+    )
 })
 
 
 
 const updatePlaylist = asyncHandler ( async (req, res) => {
-  
+  const { name, description } = req.body;
+
+  if ((!name || name.trim() === "") && (!description || description.trim() === "")) {
+    throw new ApiError(400, "At least one non-empty field is required");
+  }
+
+  const { playlistId } = req.params;
+  if (!isValidObjectId(playlistId)) {
+    throw new ApiError(400, "Invalid playlist ID")
+  }
+
+  const updateData = {};
+  if (name) updateData.name = name;
+  if (description) updateData.description = description;
+
+  const updatedPlaylist = await Playlist.findOneAndUpdate(
+    { _id: playlistId, owner: req.user?._id },
+    { $set: updateData },
+    { new: true }
+  )
+
+  if (!updatedPlaylist) {
+    throw new ApiError(404, "Playlist not found or unauthorized to update.")
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, updatedPlaylist, "Playlist updated successfully.")
+    )
 })
 
 
--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { createPlaylist } from "../controllers/playlist.controller.js";
+import { createPlaylist, deletePlaylist, updatePlaylist } from "../controllers/playlist.controller.js";
 
 const router = Router();
 router.use(verifyJWT)
 
 router.route("/").post(createPlaylist)
+router.route("/:playlistId")
+  .delete(deletePlaylist)
+  .patch(updatePlaylist)
 
 export default router;
