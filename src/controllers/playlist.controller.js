0d3686de6d1b7938a17055d5df1ca12c6bdb9c55
ayuxi03 <ayushimoitra03@gmail.com>
2025-09-26 11:11:13 +0530

    Minor fix

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -1,4 +1,4 @@
-import mongoose, { isValidObjectId } from "mongoose";
+import { isValidObjectId } from "mongoose";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { ApiError } from "../utils/ApiError.js";
