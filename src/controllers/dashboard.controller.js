996bfd16490acdb0f572bf783d945f0ac41d6efd
ayuxi03 <ayushimoitra03@gmail.com>
2025-10-01 23:36:42 +0530

    Add getChannelVideos functionality

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, { isValidObjectId } from "mongoose";
 import { Video } from "../models/video.model.js";
 import { User } from "../models/user.model.js";
 import { Subscription } from "../models/subscription.model.js";
@@ -6,13 +6,21 @@ import { Like } from "../models/like.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { isValidObjectId } from "mongoose";
 
 const getChannelStats = asyncHandler(async (req, res) => {
 
 });
 
 const getChannelVideos = asyncHandler(async (req, res) => {
+  const { channelId } = req.user?._id;
+  if (!isValidObjectId(channelId)) {
+    throw new ApiError(400, "Invalid Channel ID");
+  }
 
+  const videos = await Video
+    .find({ channelId: channelId })
+    .sort({ createdAt: -1 });
 });
 
 
