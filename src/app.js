8465cabea4d9b6da7414efa54589dc5c8fc82d1b
ayuxi03 <ayushimoitra03@gmail.com>
2025-10-03 22:18:38 +0530

    Add routes for dashboard and healthcheck endpoints to app.js

--- src/app.js
+++ src/app.js
@@ -22,6 +22,8 @@ import commentRouter from "./routes/comment.routes.js"
 import likeRouter from "./routes/like.routes.js"
 import playlistRouter from "./routes/playlist.routes.js"
 import subscriptionRouter from "./routes/subscription.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
+import healthcheckRouter from "./routes/healthcheck.routes.js"
 
 // Routes declaration
 app.use("/api/v1/users", userRouter)
@@ -31,6 +33,8 @@ app.use("/api/v1/comments", commentRouter)
 app.use("/api/v1/likes", likeRouter)
 app.use("/api/v1/playlists", playlistRouter)
 app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/dashboard", dashboardRouter)
+app.use("/api/v1/healthcheck", healthcheckRouter)
 
 //http://localhost:8000/api/v1/users/
 
