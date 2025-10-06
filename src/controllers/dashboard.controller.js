94387fa2c4a23141988d3d8cddaa604ab0982a64
ayuxi03 <ayushimoitra03@gmail.com>
2025-10-06 22:59:33 +0530

    Refactor getChannelStats to optimize database queries and improve performance

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,9 +1,9 @@
 import mongoose, { isValidObjectId } from "mongoose";
 import { Video } from "../models/video.model.js";
-import { User } from "../models/user.model.js";
 import { Subscription } from "../models/subscription.model.js";
 import { Like } from "../models/like.model.js";
 import { Tweet } from "../models/tweet.model.js";
+import { Comment } from "../models/comment.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
@@ -11,31 +11,59 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 const getChannelStats = asyncHandler(async (req, res) => {
   const userId = req.user?._id;
 
-  const totalVideos = await Video.countDocuments({ owner: userId });
+  const [ totalVideos, totalSubscribers ] = await Promise.all([
+    Video.countDocuments({ owner: userId }),
+    Subscription.countDocuments({ channel: userId })
+  ])
 
-  const totalSubscribers = await Subscription.countDocuments({ channel: userId });
+  const [ videoIds, tweetIds, commentIds ] = await Promise.all([
+    Video.find({ owner: userId }).distinct("_id"),
+    Tweet.find({ owner: userId }).distinct("_id"),
+    Comment.find({ owner: userId }).distinct("_id")
+  ])
 
-  const totalVideoLikes = await Like.countDocuments({ 
-    video: {
-      $in: await Video.find({ owner: userId }).distinct("_id")
-    } 
-  });
+  const [ totalVideoLikes, totalTweetLikes, totalCommentLikes ] = await Promise.all([
+    videoIds.length ? Like.countDocuments({ video: { $in: videoIds } }) : 0,
+    tweetIds.length ? Like.countDocuments({ tweet: { $in: tweetIds } }) : 0,
+    commentIds.length ? Like.countDocuments({ comment: { $in: commentIds } }) : 0
+  ])
 
-  const totalTweetLikes = await Like.countDocuments({ 
-    tweet: {
-      $in: await Tweet.find({ owner: userId }).distinct("_id")
-    } 
-  });
+  const views = await Video.aggregate([
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId(userId)
+      }
+    },
+    {
+      $group: {
+        _id: null,
+        totalViews: { $sum: "$views" }
+      }
+    }
+  ])
 
-  const totalCommentLikes = await Like.countDocuments({ 
-    comment: {
-      $in: await Comment.find({ owner: userId }).distinct("_id")
-    } 
-  });
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          totalVideos,
+          totalSubscribers,
+          totalVideoLikes,
+          totalTweetLikes,
+          totalCommentLikes,
+          totalViews: views[0]?.totalViews || 0
+        },
+        "Channel stats fetched successfully"
+      )
+    )
 });
 
+
+
 const getChannelVideos = asyncHandler(async (req, res) => {
-  const channelId = req.user?._id;
+  const userId = req.user?._id;
   if (!isValidObjectId(channelId)) {
     throw new ApiError(400, "Invalid Channel ID");
   }
